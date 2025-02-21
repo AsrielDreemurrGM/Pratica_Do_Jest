4e26ec4074d3f33e21e067b5e07ddb4aa19c059f
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2025-02-21 15:18:44 -0300

    Criado Teste de Inserção de Dois Comentários
    - Adicionado Alt Para a Imagem do Post;
    - Adicionado IDs de Teste aos Componentes Textarea e Button;
    - Adicionado Script Para Verificar o Coverage;

--- src/components/PostComments/__tests__/PostComments.test.tsx
+++ src/components/PostComments/__tests__/PostComments.test.tsx
@@ -1,5 +1,4 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '..';
 import PostComment from '..';
 
 describe('Teste para o componente PostComment', () => {
@@ -7,4 +6,20 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+    test('Deve testar a inserção de dois comentários', () => {
+        render(<PostComment/>);
+
+        const textarea = screen.getByTestId('comment-textarea');
+        const button = screen.getByTestId('btn-add-comment');
+        
+        fireEvent.change(textarea, {target: {value: 'Pessoa Normal: É o Batman!'}});
+        fireEvent.click(button);
+
+        expect(screen.getByText('Pessoa Normal: É o Batman!')).toBeInTheDocument();
+
+        fireEvent.change(textarea, {target: {value: 'Coringa: É o Morcego!'}});
+        fireEvent.click(button);
+
+        expect(screen.getByText('Coringa: É o Morcego!')).toBeInTheDocument();
+    })
 });
